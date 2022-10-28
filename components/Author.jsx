7302ed771e8e0e2cb4ecb2a3f9885b2d725b3772
sugarkane <viet.quyenbui@gmail.com>
2022-10-29 00:54:31 +0700

    Adding author's info in the post
    Author's image, name and bio are added to the end of each post

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "next/image";
 
-const Author = () => {
-  return <div>Author</div>;
-};
+const Author = ({ author }) => (
+  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+    <div className="absolute left-0 right-0 -top-14">
+      <Image
+        unoptimized
+        alt={author.name}
+        height="100"
+        width="100"
+        className="mx-auto rounded-full"
+        src={author.photo.url}
+      />
+    </div>
+    <h3 className="text-black my-4 text-xl font-bold">{author.name}</h3>
+    <p className="text-black text-ls">{author.bio}</p>
+  </div>
+);
 
 export default Author;
