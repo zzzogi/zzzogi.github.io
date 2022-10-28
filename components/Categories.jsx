226d46663242de4dbd761dd1c950ffcacd8a9e05
sugarkane <viet.quyenbui@gmail.com>
2022-10-28 18:12:26 +0700

    Adding the ability to display posts

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCategories } from "../services";
 
 const Categories = () => {
-  return <div>Post Widget</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Danh mục</h3>
+      {categories.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,12 +14,12 @@ export default function Home({ posts }: any) {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post: any) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget />
+            <PostWidget categories={undefined} slug={undefined} />
             <Categories />
           </div>
         </div>
