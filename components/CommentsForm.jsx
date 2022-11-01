48eba9a3cf1fd696df2b74d0209b1acaaa893640
sugarkane <viet.quyenbui@gmail.com>
2022-11-01 16:27:19 +0700

    Finishing the project
    Nice

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-const CommentsForm = () => {
-  return <div>CommentsForm</div>;
+import { submitComment } from "../services";
+
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  }, []);
+
+  const handleCommentSubmisstion = () => {
+    setError(false);
+
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
+
+    if (storeData) {
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
+    } else {
+      window.localStorage.removeItem("name");
+      window.localStorage.removeItem("email");
+    }
+
+    submitComment(commentObj).then((res) => {
+      setShowSuccessMessage(true);
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 3000);
+    });
+  };
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Để lại bình luận
+      </h3>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outlint-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Bình luận của bạn"
+          name="comment"
+        />
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outlint-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Tên của bạn"
+          name="Name"
+        />
+        <input
+          type="text"
+          ref={emailEl}
+          className="py-2 px-4 outlint-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <div>
+          <input
+            ref={storeDataEl}
+            type="checkbox"
+            id="storeData"
+            name="storeData"
+            value="true"
+          />
+          <label
+            className="text-gray-500 cursor-pointer ml-2"
+            htmlFor="storeData"
+          >
+            Lưu thông tin của tôi
+          </label>
+        </div>
+      </div>
+      {error && (
+        <p className="text-xs text-red-500">Vui lòng điền đủ thông tin</p>
+      )}
+      <div className="mt-8">
+        <button
+          type="button"
+          onClick={handleCommentSubmisstion}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-4"
+        >
+          Gửi bình luận
+        </button>
+        {showSuccessMessage && (
+          <span className="text-xl float-right font-semibold mt-3 text-green-500">
+            Bình luận của bạn đang chờ duyệt
+          </span>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CommentsForm;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,20 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import { FeaturedPosts } from "../sections/index";
 import { PostCard, Categories, PostWidget } from "../components";
 import { getPosts } from "../services";
 
-export default function Home({ posts }: any) {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Blog của Quyền</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post: any) => (
-            <PostCard post={post.node} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard key={index} post={post.node} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget categories={undefined} slug={undefined} />
+            <PostWidget />
             <Categories />
           </div>
         </div>
