cbbdf07e18ab4b7b931e1d18819a8766e67f77b0
sugarkane <viet.quyenbui@gmail.com>
2022-11-01 17:32:37 +0700

    Final touch to deploy the app

--- components/index.js
+++ components/index.js
@@ -9,3 +9,4 @@ export { default as CommentsForm } from "./CommentsForm";
 export { default as PostDetail } from "./PostDetail";
 export { default as FeaturedPostCard } from "./FeturedPostCard";
 export { default as Loader } from "./Loader";
+export { default as AdjacentPostCard } from "./AdjacentPostCard";
